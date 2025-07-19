e1f55d65fcbaa3fed007ea6ed3834448356eb04e
Sameer-A01 <tubesameer786@gmail.com>
2025-07-19 16:44:23 +0530

    Initial commit - pushing frontend code

--- server.js
+++ server.js
@@ -4,17 +4,13 @@ dotenv.config();
 import cors from 'cors';
 
 import authRouter from "./routes/auth.js";
-
 import userRouter from './routes/user.js';
-
-// import dashboardRouter from './routes/dashboard.js';
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import attendanceRouter from './routes/attendance.js';
 import studentRouter from './routes/student.js';
-
 import biometricRouter from './routes/biometric.js';
 
-
+import { handleAttendancePush } from './controllers/biometricController.js';
 
 const app = express();
 
@@ -22,21 +18,22 @@ app.use(express.static('public'));
 app.use(cors());
 app.use(express.json());
 
+/** 👇 Biometric device will send logs to '/' via IP mode */
+app.post('/', handleAttendancePush);
 
-// app.use("/api/dashboard", dashboardRouter);
+/** 👇 REST API routes */
 app.use("/api/auth", authRouter);
-
 app.use("/api/users", userRouter);
-
 app.use('/api/attendance', attendanceRouter);
 app.use('/api/students', studentRouter);
 app.use('/api/biometric', biometricRouter);
 
+/** 👇 For browser test */
 app.get('/', (req, res) => {
   res.send('✅ Biometric Attendance API is running.');
 });
 
-// ✅ Start server and polling
+/** 🚀 Start server */
 app.listen(process.env.PORT, () => {
   connectToMongoDB();
   console.log(`🚀 Server Running on port ${process.env.PORT}`);
